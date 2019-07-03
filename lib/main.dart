d6ad0b45c809b1583a9dc675c4bc03eb497e4335
JerryZhu <773749686@qq.com>
2019-07-03 15:53:23 +0800

    完成简单ListView和页面跳转

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,11 @@ void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-    var wordPair = new WordPair.random();
     return new MaterialApp(
       title: 'flutter demo',
+      theme: new ThemeData(
+        primaryColor: Colors.amber,
+      ),
       home: new RandomWords(),
     );
   }
@@ -21,67 +22,85 @@ class RandomWords extends StatefulWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
-
   final _suggestions = <WordPair>[];
+  final _saved = Set<WordPair>();
+
   final _biggerFont = const TextStyle(fontSize: 18.0);
 
   @override
   Widget build(BuildContext context) {
-
     return new Scaffold(
       appBar: new AppBar(
-
         title: new Text("flutter demo"),
-
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved)
+        ],
       ),
-
       body: _buildSuggesttions(),
-
     );
   }
 
-  Widget _buildSuggesttions(){
-
+  Widget _buildSuggesttions() {
     return new ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return new Divider();
 
-      padding:const EdgeInsets.all(16.0),
+          final index = i ~/ 2;
 
-      itemBuilder: (context,i){
-
-        if(i.isOdd) return new Divider();
-
-        final index = i ~/ 2;
-
-        print("i === $i");
-
-        print("index === $index");
-
-        if(index >= _suggestions.length){
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-
-        return _buidRow(_suggestions[index]);
-
-      }
-
-
-    );
-
-
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buidRow(_suggestions[index]);
+        });
   }
 
   Widget _buidRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
 
+    print("_buidRow alreadySaved : $alreadySaved");
     return new ListTile(
-
       title: new Text(
-
         pair.asPascalCase,
-
-        style: _biggerFont,),
-
-
+        style: _biggerFont,
+      ),
+      trailing: new Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : Colors.blue,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
 
+  void _pushSaved() {
+    Navigator.of(context).push(new MaterialPageRoute(
+      builder: (context) {
+        final titles = _saved.map((pair) {
+          return new ListTile(
+            title: new Text(
+              pair.asPascalCase,
+              style: _biggerFont,
+            ),
+          );
+        });
+        final divies =
+            ListTile.divideTiles(context: context, tiles: titles).toList();
+
+        return new Scaffold(
+          appBar: new AppBar(
+            title: new Text("saved page"),
+          ),
+          body: new ListView(children: divies),
+        );
+      },
+    ));
+  }
 }
